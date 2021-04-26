4e1eee68806dd2fd6ef971ec8c9c51c5c5fcc2b0
Oreofreakshake <yoosufyazak@gmail.com>
2021-04-26 23:32:22 +0500

    commit

---
+++ Fodifulhu.py
@@ -0,0 +1,36 @@
+import os
+import random
+
+import discord
+from discord.ext import commands
+
+TOKEN = open("token.txt", "r").readline()
+
+bot = commands.Bot(command_prefix=".")
+bot.remove_command("help")
+
+
+bot.load_extension(f"cogs.Ping")
+
+
+bot.load_extension(f"cogs.Help")
+
+
+@commands.command()
+async def say(ctx, *args):
+
+    if len(args) > 0:
+        await ctx.send(" ".join(args))
+    else:
+        await ctx.send(f"refer to the help command")
+
+
+bot.add_command(say)
+
+
+@bot.command()
+async def add_command():
+    bot.add_command(help)
+
+
+bot.run(TOKEN)
---
+++ cogs/Ping.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+
+
+class Ping(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send(f"pong your ping is")
+
+
+def setup(bot):
+    bot.add_cog(Ping(bot))
